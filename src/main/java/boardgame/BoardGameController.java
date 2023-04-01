ec66158155d8434f7a701885150ae3b9f62897ba
Péter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2023-04-01 15:12:31 +0200

    Initial import

---
+++ src/main/java/boardgame/BoardGameController.java
@@ -0,0 +1,55 @@
+package boardgame;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class BoardGameController {
+
+    @FXML
+    private GridPane board;
+
+    @FXML
+    private void initialize() {
+        for (var i = 0; i < board.getRowCount(); i++) {
+            for (var j = 0; j < board.getColumnCount(); j++) {
+                var square = createSquare();
+                board.add(square, j, i);
+            }
+        }
+    }
+
+    private StackPane createSquare() {
+        var square = new StackPane();
+        square.getStyleClass().add("square");
+        var piece = new Circle(50);
+        piece.setFill(Color.TRANSPARENT);
+        square.getChildren().add(piece);
+        square.setOnMouseClicked(this::handleMouseClick);
+        return square;
+    }
+
+    @FXML
+    private void handleMouseClick(MouseEvent event) {
+        var square = (StackPane) event.getSource();
+        var row = GridPane.getRowIndex(square);
+        var col = GridPane.getColumnIndex(square);
+        System.out.printf("Click on square (%d,%d)%n", row, col);
+        var coin = (Circle) square.getChildren().get(0);
+        coin.setFill(nextColor((Color) coin.getFill()));
+    }
+
+    private Color nextColor(Color color) {
+        if (color == Color.TRANSPARENT) {
+            return Color.RED;
+        }
+        if (color == Color.RED) {
+            return Color.BLUE;
+        }
+        return Color.TRANSPARENT;
+    }
+
+}
